2238d26839345b81f94f0f3fe192d123bf7e871e
sesi200 <sev-sif@lula.ch>
2020-03-20 16:26:51 +0100

    add auctionhouse

--- contract/auctionhouse.sol
+++ contract/auctionhouse.sol
@@ -9,8 +9,8 @@ contract Auction {
     uint public current_max_bid;
     uint public min_bid;
    
-   constructor(string memory auction_description, uint auction_duration_seconds, uint minimum_bid) public {
-       seller = msg.sender;
+   constructor(address payable _seller, string memory auction_description, uint auction_duration_seconds, uint minimum_bid) public {
+       seller = _seller;
        description = auction_description;
        end_timestamp = now + auction_duration_seconds;
        current_max_bid = 0;
@@ -45,4 +45,17 @@ contract Auction {
    function close_auction() public auction_ended {
        seller.transfer(current_max_bid);
    }
+}
+
+contract AuctionHouse {
+    Auction[] public auctions;
+    
+    function createAuction() external {
+        Auction newAuction = new Auction(msg.sender, "desc", 60, 0);
+        auctions.push(newAuction);
+    }
+    
+    function getAuctionAdrress(uint auction_index) public view returns(address) {
+        return address(auctions[auction_index]);
+    }
 }

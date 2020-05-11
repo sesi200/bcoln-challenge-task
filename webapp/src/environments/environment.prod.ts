b762d04d1fc5744b1bbd1ffe248f4a22216036d8
Noah Chavannes <noah.chavannes93@gmail.com>
2020-05-11 14:49:43 +0200

    added rinkby to prod env

--- webapp/src/environments/environment.prod.ts
+++ webapp/src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: true,
   mainAuctionHouseAddress: '0x0000000000000000000000000000000000000000',
-  ropstenAuctionHouseAddress: '0x0000000000000000000000000000000000000000',
+  ropstenAuctionHouseAddress: '0x2b440b145c6923a99339e9dc1c648d9c830f82d7',
+  rinkebyAuctionHouseAddress: '0xe7e398dcd33ce741f8fd240ca2074b6c0b364dbc',
   privateAuctionHouseAddress: '0x0000000000000000000000000000000000000000'
 };
